8d58210730e1e9602bd77cb5b172a24361a8b42e
Christian Heimke <christian.heimke@loumaris.com>
2022-11-29 11:11:56 +0100

    added backend

--- provider.tf
+++ provider.tf
@@ -5,7 +5,15 @@ terraform {
       version = "3.25.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "christian-heimke-terraformstate-rg"
+    storage_account_name = "heimketerraformstate"
+    container_name       = "terraform"
+    key                  = "terraformprojectstate.tfstate"
+  }
 }
+
 
 provider "azurerm" {
   subscription_id = "ea6e6692-4d05-4c5b-9909-51c7dc5f5c2b"
